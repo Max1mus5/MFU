3f25d589aff22dda489df8a68d7134f0777b31c5
jeronimo <jeronimor.dev@gmail.com>
2025-05-14 09:53:01 +0000

    Improved MFU algorithm logging to show resource type and counter values

--- src/core/mfu_algorithm.py
+++ src/core/mfu_algorithm.py
@@ -29,11 +29,21 @@ class MFUAlgorithm:
         max_counter = -1
         max_index = -1
         print("MFUAlgorithm: get_replacement_index called")
-        print("Resources:", resources)
+        
+        # Print resources with useful information
+        print("Resources:")
+        for i, resource in enumerate(resources):
+            print(f"  [{i}] Type: {resource.type}, Counter: {resource.counter}")
+        
         for i, resource in enumerate(resources):
             if resource.counter > max_counter:
                 max_counter = resource.counter
                 max_index = i
-                print(f"New max counter found: {max_counter} at index {max_index}")
+                print(f"New max counter found: {max_counter} at index {max_index} (Type: {resource.type})")
         
+        if max_index >= 0:
+            print(f"Selected resource for replacement: [{max_index}] Type: {resources[max_index].type}, Counter: {resources[max_index].counter}")
+        else:
+            print("No resource selected for replacement")
+            
         return max_index
